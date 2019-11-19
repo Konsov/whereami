03f343da1419dcf0d5c068da431b72a366f02cfb
LucaBianco94 <luca3.bianco@mail.polimi.it>
2019-11-19 16:00:21 +0100

    firebase

--- services/firebase.js
+++ services/firebase.js
@@ -2,14 +2,14 @@
 import * as firebase from "firebase";
 
 const config = {
-    apiKey: "AIzaSyAGYdr7LvlyGzDHZTs0VEzuD1gmGOvIAPI",
-    authDomain: "whereami-a1509.firebaseapp.com",
-    databaseURL: "https://whereami-a1509.firebaseio.com",
-    projectId: "whereami-a1509",
-    storageBucket: "whereami-a1509.appspot.com",
-    messagingSenderId: "598344557393",
-    appId: "1:598344557393:web:929ad11caf401a5aa2a970",
-    measurementId: "G-3MK8RNF0BG"
+    apiKey: "AIzaSyD0ylq1lq09wMlsr2GEgbfhj_9NKkrJgRw",
+    authDomain: "fir-chat-3d414.firebaseapp.com",
+    databaseURL: "https://fir-chat-3d414.firebaseio.com",
+    projectId: "fir-chat-3d414",
+    storageBucket: "fir-chat-3d414.appspot.com",
+    messagingSenderId: "543229345691",
+    appId: "1:543229345691:web:092028e0969cd9f25f6b3e",
+    measurementId: "G-0J0W0JKY1J"
 };
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
