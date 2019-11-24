c121b58a226d1306c6b6c5fa18f6fbc1e24b816b
Konsov <44020258+Konsov@users.noreply.github.com>
2019-11-24 23:28:09 +0100

    trigger 1

--- fb-functions/functions/index.js
+++ fb-functions/functions/index.js
@@ -5,18 +5,63 @@ admin.initializeApp(functions.config().firebase);
 
 
 exports.createRandomGame = functions.region('europe-west1').database
-.ref('/waitingRoom/{playerID}')
-.onCreate((snapshot, context) => {
-   
-   console.log(context.params.playerID)
-  
-   const player = snapshot.val()
-   const ID = player.user
+.ref('/waitingRoom')
+.onUpdate((snapshot, context) => {
+
+    console.log('snapshot')
+    console.log(snapshot)
+    
+    console.log('snaposhot.after.val')
+    console.log(snapshot.after.val())
+
+    console.log('snapshot.toJson')
+    console.log(snapshot.after.toJSON())
+
+    console.log('snaposhot.numChildren()')
+    console.log(snapshot.after.numChildren())
+
+
+    console.log('snaposhot.hasChild')
+    console.log(snapshot.after.hasChild('fxco4umom8XDKprM7aANkRObX8j2'))
+
+    const playersInWatingRoom = snaposhot.after.numChildren()
+    
+    if(playersInWatingRoom >= 2){
+
+    const player1 =
+    const player2 =
+
+    }
+/*
+    const playersInWatingRoom = snapshot.ref.parent.once('value', (datasnapshot) => {
+        const numberOfChildren = datasnapshot.numChildren()
+        return numberOfChildren
+    });
+    console.log(playersInWatingRoom)
+
+
+    const player = snapshot.val()
+    const ID = player.user
  
-   return snapshot.ref.parent.parent.child('games').child('game_1').set({
+    return snapshot.ref.parent.parent.child('games').child('game_1').set({
                 player1: ID,
                 player2: 'eeeh chissà come si fa',
             });
+            
 
+
+  return snapshot.ref.parent.once('value').then((datasnapshot) => {
+        const numberOfChildren = datasnapshot.numChildren()
+        return numberOfChildren
+    }).then((usersInWaitingRoom) => {
+        console.log(usersInWaitingRoom)    
+        if(usersInWaitingRoom > 100)
+          // Do Something
+        else
+          // Do Something Else
+    }); 
+
+
+*/
 
 });
