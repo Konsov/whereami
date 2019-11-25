b41f3ca86c384cb72fa2d78018655b21dac25dab
LucaBianco94 <luca3.bianco@mail.polimi.it>
2019-11-25 12:37:49 +0100

    triggerAssurdo

--- fb-functions/functions/index.js
+++ fb-functions/functions/index.js
@@ -3,65 +3,112 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+exports.cleanWaitingRoom = functions.region('europe-west1').database
+        .ref('/Games/{GameID}')
+        .onCreate((snaposhot,context) => {
+            const play1 = snaposhot.val().player1          
+
+            console.log(play1)
+            const play2 = snaposhot.val().player2
+            console.log(play2)
+
+            snaposhot.ref.parent.parent.child('waitingRoom').child(play1).remove().then(function() {
+                console.log("Remove Player 1.")
+              })
+              .catch(function(error) {
+                console.log("Remove failed: " + error.message)
+              });
+
+            snaposhot.ref.parent.parent.child('waitingRoom').child(play2).remove().then(function() {
+                console.log("Remove Player 2.")
+              })
+              .catch(function(error) {
+                console.log("Remove failed: " + error.message)
+              });
+              
+
+              return null
+
+        })
+
+
+
+
 
 exports.createRandomGame = functions.region('europe-west1').database
-.ref('/waitingRoom')
-.onUpdate((snapshot, context) => {
+    .ref('/waitingRoom/{userID}')
+    .onCreate((snaposhot, context) => {
 
-    console.log('snapshot')
-    console.log(snapshot)
-    
-    console.log('snaposhot.after.val')
-    console.log(snapshot.after.val())
-
-    console.log('snapshot.toJson')
-    console.log(snapshot.after.toJSON())
-
-    console.log('snaposhot.numChildren()')
-    console.log(snapshot.after.numChildren())
+        const ID = context.params.userID
 
 
-    console.log('snaposhot.hasChild')
-    console.log(snapshot.after.hasChild('fxco4umom8XDKprM7aANkRObX8j2'))
+        return snaposhot.ref.parent.once('value').then(res => {
 
-    const playersInWatingRoom = snaposhot.after.numChildren()
-    
-    if(playersInWatingRoom >= 2){
+            console.log('numChildren')
+            console.log(res.numChildren())
 
-    const player1 =
-    const player2 =
+            if (res.numChildren() < 2) {
+                return null
+            } else {
 
-    }
-/*
-    const playersInWatingRoom = snapshot.ref.parent.once('value', (datasnapshot) => {
-        const numberOfChildren = datasnapshot.numChildren()
-        return numberOfChildren
-    });
-    console.log(playersInWatingRoom)
+                for (var c in res.toJSON()) {
+                    if (c === ID) {
+                        console.log('trovato')
+
+                    } else {
+                        const ID2 = c
+                        return res.ref.parent.child('Games').child(ID + ID2).set({
+                            player1:ID, 
+                            player2: ID2
+                        });
+
+                    }
+                }
+            }
 
 
-    const player = snapshot.val()
-    const ID = player.user
- 
-    return snapshot.ref.parent.parent.child('games').child('game_1').set({
-                player1: ID,
-                player2: 'eeeh chissà come si fa',
+        }).catch(err => {
+
+        });
+
+        // const uppercase = original.toUpperCase();
+
+
+
+
+        //const playersInWatingRoom = snaposhot.numChildren()
+
+
+        /*
+            const playersInWatingRoom = snapshot.ref.parent.once('value', (datasnapshot) => {
+                const numberOfChildren = datasnapshot.numChildren()
+                return numberOfChildren
             });
-            
+            console.log(playersInWatingRoom)
+        
+        
+            const player = snapshot.val()
+            const ID = player.user
+         
+            return snapshot.ref.parent.parent.child('games').child('game_1').set({
+                        player1: ID,
+                        player2: 'eeeh chissà come si fa',
+                    });
+                    
+        
+        
+          return snapshot.ref.parent.once('value').then((datasnapshot) => {
+                const numberOfChildren = datasnapshot.numChildren()
+                return numberOfChildren
+            }).then((usersInWaitingRoom) => {
+                console.log(usersInWaitingRoom)    
+                if(usersInWaitingRoom > 100)
+                  // Do Something
+                else
+                  // Do Something Else
+            }); 
+        
+        
+        */
 
-
-  return snapshot.ref.parent.once('value').then((datasnapshot) => {
-        const numberOfChildren = datasnapshot.numChildren()
-        return numberOfChildren
-    }).then((usersInWaitingRoom) => {
-        console.log(usersInWaitingRoom)    
-        if(usersInWaitingRoom > 100)
-          // Do Something
-        else
-          // Do Something Else
-    }); 
-
-
-*/
-
-});
+    });
