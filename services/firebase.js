d174276589af1429686b88a900f7d4878829eda0
Konsov <44020258+Konsov@users.noreply.github.com>
2022-07-03 15:49:48 +0200

    Update firebase.js

--- services/firebase.js
+++ services/firebase.js
@@ -2,14 +2,7 @@
 import * as firebase from "firebase";
 
 const config = {
-    apiKey: "AIzaSyD0ylq1lq09wMlsr2GEgbfhj_9NKkrJgRw",
-    authDomain: "fir-chat-3d414.firebaseapp.com",
-    databaseURL: "https://fir-chat-3d414.firebaseio.com",
-    projectId: "fir-chat-3d414",
-    storageBucket: "fir-chat-3d414.appspot.com",
-    messagingSenderId: "543229345691",
-    appId: "1:543229345691:web:092028e0969cd9f25f6b3e",
-    measurementId: "G-0J0W0JKY1J"
+    //qui api e config database, cancellato perchè pubblico su git
 };
 
 
